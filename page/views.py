551b64c3ca3ad4cb0a4a219fb822ee2790cdddb1
KIM MINJI <sml07113@gmail.com>
2021-02-22 00:38:51 +0900

    [4차시] 4. 세부 페이지 구현하기

--- page/views.py
+++ page/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from page.models import Post
 
 # Create your views here.
+
+# 홈 화면
 def home(request):
     posts = Post.objects.all()
     return render(request, 'home.html', {'posts':posts})
+
+# 디테일 화면
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'detail.html', {'post':post})
